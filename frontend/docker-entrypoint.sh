77eabea43970f91eea0a378c06182b17053056fd
xiadnoring <timurtuty132@gmail.com>
2026-07-02 13:01:05 +0300

    fix:frontend

--- frontend/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Если node_modules пустой, копируем из образа
-if [ ! -d "/app/node_modules" ] || [ -z "$(ls -A /app/node_modules 2>/dev/null)" ]; then
-    echo "Installing dependencies..."
-    cp -r /app/node_modules_cached/* /app/node_modules/
-fi
-
-exec "$@"
